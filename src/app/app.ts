8e796f587c847e7c6261837fed44213d54745eb8
paulotruly <paulomsmsms@gmail.com>
2025-10-01 10:43:00 -0300

    upando

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+export class App {
+  
+}
